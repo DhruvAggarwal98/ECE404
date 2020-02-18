c7fee0886364fe36fb1bd4da754f0d32619c3cb4
Dhruv Aggarwal <terrificplanes98@gmail.com>
2020-02-18 01:34:46 -0500

    didnt finish

--- HW4/AES.py
+++ HW4/AES.py
@@ -6,7 +6,7 @@ import sys
 from BitVector import *
 
 AES_modulus = BitVector(bitstring='100011011')
-subBytesTable = []                                                  # for encryption
+subBytesTable = []                                              # for encryption
 invSubBytesTable = []    
 
 def gen_key_schedule_256(key_bv):
@@ -64,6 +64,21 @@ def gen_subbytes_table():
         subBytesTable.append(int(a))
     return subBytesTable
 
+table1 = [ 99, 124, 119, 123, 242, 107, 111, 197,  48,   1, 103,  43, 254, 215, 171, 118, 202, 130, 201, 125,
+                 250,  89,  71, 240, 173, 212, 162, 175, 156, 164, 114, 192, 183, 253, 147,  38,  54,  63, 247, 204,
+                  52, 165, 229, 241, 113, 216,  49,  21,   4, 199,  35, 195,  24, 150,   5, 154,   7,  18, 128, 226,
+                 235,  39, 178, 117,   9, 131,  44,  26,  27, 110,  90, 160,  82,  59, 214, 179,  41, 227,  47, 132,
+                  83, 209,   0, 237,  32, 252, 177,  91, 106, 203, 190,  57,  74,  76,  88, 207, 208, 239, 170, 251,
+                  67,  77,  51, 133,  69, 249,   2, 127,  80,  60, 159, 168,  81, 163,  64, 143, 146, 157,  56, 245,
+                 188, 182, 218,  33,  16, 255, 243, 210, 205,  12,  19, 236,  95, 151,  68,  23, 196, 167, 126,  61,
+                 100,  93,  25, 115,  96, 129,  79, 220,  34,  42, 144, 136,  70, 238, 184,  20, 222,  94,  11, 219,
+                 224,  50,  58,  10,  73,   6,  36,  92, 194, 211, 172,  98, 145, 149, 228, 121, 231, 200,  55, 109,
+                 141, 213,  78, 169, 108,  86, 244, 234, 101, 122, 174,   8, 186, 120,  37,  46,  28, 166, 180, 198,
+                 232, 221, 116,  31,  75, 189, 139, 138, 112,  62, 181, 102,  72,   3, 246,  14,  97,  53,  87, 185,
+                 134, 193,  29, 158, 225, 248, 152,  17, 105, 217, 142, 148, 155,  30, 135, 233, 206,  85,  40, 223,
+                 140, 161, 137,  13, 191, 230,  66, 104,  65, 153,  45,  15, 176,  84, 187,  22]
+
+
 def gee(keyword, round_constant, byte_sub_table):
 
     '''
@@ -96,12 +111,13 @@ def genTables():
         check = b.gf_MI(AES_modulus, 8)
         b = check if isinstance(check, BitVector) else 0
         invSubBytesTable.append(int(b))
+    
+    
 
 def sub_bytes(array):
-    new_table = hex(gen_subbytes_table())
-    for each in range(0,3):
-        for each2 in range(0,3):
-            array[each][each2]= hex(new_table[int(array[each][each2],0)])
+    for each in range(4):
+        for each2 in range(4):
+            array[each][each2]= BitVector(intVal=table1[int(array[each][each2])])
     return array
 
 def shift_rows(array):
@@ -109,6 +125,22 @@ def shift_rows(array):
         array[each] = array[each][each:] + array[each][:each]
     return array
 
+def mix_columns(array):
+    w = BitVector(bitstring='00000010')
+    x = BitVector(bitstring = '00000011')
+    y= BitVector(bitstring = '00000001')
+    z = BitVector(bitstring = '00000001')
+    array2= [w,x,y,z]
+    temp_array = array
+    for each in range(4):
+        rotated = array2[-each:]+array2[:-each]
+        for each2 in range(4):
+            final = BitVector(bitstring = '00000000')
+            for each3 in range(4):
+                half_final = rotated[each3].gf_multiply_modular(temp_array[each3][each2],AES_modulus,8)
+                final = final ^ half_final
+            array[each][each2]= final
+    return array         
 
 def encrypt(message,key,output):
     file_in = open(key,"r")
@@ -117,21 +149,31 @@ def encrypt(message,key,output):
     key_bv = BitVector(textstring = file_in.read())
     state_array = [[0 for i in range(4)] for i in range(4)]
     next_array  = [[0 for i in range(4)] for i in range(4)]
-    round_key = gen_round_keys(key_bv)    
+    key_words = gen_key_schedule_256(key_bv) 
     while (bv.more_to_read):
         bitvec = bv.read_bits_from_file( 128 )
         if bitvec.length() > 0:
             if bitvec.length() < 128:
                 bitvec.pad_from_right(128-bitvec.length())  
-            bitvec ^= round_key[0]
+            for each in range(4):
+                for each2 in range(4):
+                    state_array[each][each2] = bitvec[32 * each + 8 * each2:32 * each + 8 * (each2 + 1)]
+                    state_array[each][each2] ^= key_words[each][8 * each2:8 + (8 * each2)]
             for i in range(4):
                 for j in range(4):
                     state_array[j][i]=bitvec[32*i+8*j:32*i+8*(j+1)]
-            # for each_round in range(0,13):
-                # state_array = next_array
-                # state_array=sub_bytes(state_array)
-                # state_array =shift_rows(state_array)
-                # print(state_array)
+            for each_round in range(14):
+                state_array = next_array
+                state_array=sub_bytes(state_array)
+                state_array =shift_rows(state_array)
+                if each_round != 13:
+                    state_array = mix_columns(state_array)
+                for each in range(4):
+                    for each2 in range(4):
+                        state_array[each][each2] ^= key_words[(4 * (each_round + 1)) + each][8 * each2:8 + (8 * each2)]
+            for convert in range(4):
+                for convert2 in range(4):
+                    state_array[convert][convert2].write_to_file(output1)
 
 
 if sys.argv[1] == "-e":
