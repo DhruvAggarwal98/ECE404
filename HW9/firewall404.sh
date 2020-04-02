2d1a0b3f5e0dae3c2bbe86efef5e71948002836a
Dhruv Aggarwal <terrificplanes98@gmail.com>
2020-04-02 12:48:57 -0400

    Finished

--- HW9/firewall404.sh
+++ HW9/firewall404.sh
@@ -6,9 +6,13 @@
 
 #flush all chains
 sudo iptables -F
-sudo iptables -t nat -F
-sudo iptables -t mangle -F
 sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+sudo iptables -t raw -F
+sudo iptables -t raw -X
+sudo iptables -t mangle -F
+sudo iptables -t mangle -X
 sudo iptables -t filter -F
 sudo iptables -t filter -X
 
